e2984e5271063351b3b2623e9617bbf4967e8b1e
VolkCristina <kvolk@dynak.com>
2021-02-12 04:11:15 +0000

    react anime footer [added] to the about page

---
+++ src/components/anime-footer/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Anime from "react-anime";
+import "./styles.css";
+
+export const AnimeFooter = () => (
+  <div className='anime-container'>
+    <Anime
+      easing='easeOutElastic'
+      loop={true}
+      duration={5000}
+      direction='alternate'
+      delay={(el, index) => index * 270}
+      translateX='63rem'
+      scale={[0.75, 0.9]}
+    >
+      <div className='blue' />
+      <div className='green' />
+      <div className='red' />
+    </Anime>
+  </div>
+);
--- src/pages/about-page/index.jsx
+++ src/pages/about-page/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { MotionGallery } from "../../components/motion-gallery/";
-import "./styles.css";
+import { AnimeFooter } from "../../components/anime-footer";
+import styles from "./styles.module.css";
 
 export default function AboutPage() {
   return (
-    <>
+    <div className={styles.aboutPageContainer}>
       <MotionGallery />
-    </>
+      <AnimeFooter />
+    </div>
   );
 }
