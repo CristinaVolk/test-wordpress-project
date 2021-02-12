21be92ea5fe6f5646aa7e2962a7fa685aee328ae
VolkCristina <kvolk@dynak.com>
2021-02-12 04:10:27 +0000

    the links style [updated], footer [fixed], limk to the logo img [added]

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./styles.module.css";
 import MCGLogo from "../../assets/MCGLogo.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className={styles.navbarWrapper}>
       <div className={styles.navbar}>
-        <img className={styles.logo} src={MCGLogo} alt='Logo' />
+        <NavLink to='/'>
+          <img className={styles.logo} src={MCGLogo} alt='Logo' />
+        </NavLink>
       </div>
-      <div className={styles.borderArea}></div>
     </div>
   );
 }
