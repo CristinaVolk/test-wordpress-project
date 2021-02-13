2c137fb7de2690115ef84eab56827f185f1a5821
VolkCristina <kvolk@dynak.com>
2021-02-13 03:36:58 +0000

    social media links [implemented]

--- src/components/sidebar-right/index.jsx
+++ src/components/sidebar-right/index.jsx
@@ -4,6 +4,12 @@ import InstagramIcon from "../../assets/instagram-icon.png";
 import TwitterIcon from "../../assets/twitter-icon.png";
 import styles from "./styles.module.css";
 
+const links = {
+  facebook: "https://m.facebook.com/profile.php?id=469568180296191",
+  twitter: "https://twitter.com/MyCleverGroup",
+  instagram: "https://www.instagram.com/myclevergroup/",
+};
+
 export default function SideBarRight() {
   const [language, setLanguage] = useState("En");
 
@@ -23,9 +29,27 @@ export default function SideBarRight() {
       </button>
       <div className={styles.secondChild}>Welcome to MCG</div>
       <div className={styles.thirdChild}>
-        <img src={FacebookIcon} alt='facebook' />
-        <img src={InstagramIcon} alt='instagram' />
-        <img src={TwitterIcon} alt='twitter' />
+        <a href={links.facebook}>
+          <img
+            src={FacebookIcon}
+            alt='facebook'
+            className={styles.socialMediaIcons}
+          />
+        </a>
+        <a href={links.instagram}>
+          <img
+            src={InstagramIcon}
+            alt='instagram'
+            className={styles.socialMediaIcons}
+          />
+        </a>
+        <a href={links.twitter}>
+          <img
+            src={TwitterIcon}
+            alt='twitter'
+            className={styles.socialMediaIcons}
+          />
+        </a>
       </div>
     </div>
   );
