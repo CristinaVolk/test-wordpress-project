472ed6fbfe9d3edab880264fd1578c15c63eb363
VolkCristina <kvolk@dynak.com>
2021-02-14 02:53:41 +0000

    animated balls [edited] in the footer of about us page

--- src/components/anime-footer/index.jsx
+++ src/components/anime-footer/index.jsx
@@ -3,14 +3,14 @@ import Anime from "react-anime";
 import styles from "./styles.module.css";
 
 export const AnimeFooter = () => (
-  <div>
+  <div className={styles.ballsContainer}>
     <Anime
       easing='easeOutElastic'
       loop={true}
       duration={5000}
       direction='alternate'
       delay={(el, index) => index * 270}
-      translateX='63rem'
+      translateX='40rem'
       scale={[0.15, 0.9]}
     >
       <div className={styles.first} />
