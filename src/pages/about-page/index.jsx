4ebdab875851dfa7fcad914f752371e2d339bb23
VolkCristina <kvolk@dynak.com>
2021-02-17 00:30:25 +0000

    AboutPage is responsive

--- src/pages/about-page/index.jsx
+++ src/pages/about-page/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { MotionGallery } from "../../components/motion-gallery/";
-import { AnimeFooter } from "../../components/anime-footer";
 import styles from "./styles.module.css";
 
 export default function AboutPage() {
   return (
     <div className={styles.aboutPageContainer}>
       <MotionGallery />
-      <AnimeFooter />
     </div>
   );
 }
